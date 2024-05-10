f4b283ee39ee8268b35f3b91d30da09a1a20dd3e
daemonspectre <admin@family-p.com>
2024-05-09 21:53:30 -0400

    Add files via upload

---
+++ ubuntu-install-docker.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# update and upgrade #
+apt-get update && sudo apt-get upgrade -y
+
+# install curl # 
+apt-get install curl
+
+# curl ca certs #
+curl ca-certificates
+
+# create dir for keyrings #
+install -m 0755 -d /etc/apt/keyrings
+
+# download docker GPG key #
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+
+# something about permissions #
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources and updates #
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# install :latestversion of docker #
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# install compose lastest:version #
+apt-get update
+apt-get install docker-compose-plugin
+
+# verify docker and compose install #
+docker run hello-world
+docker compose version
