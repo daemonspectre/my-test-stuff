f9d5bf4618aac694045d8493d87feb51320344ac
daemonspectre <admin@family-p.com>
2024-06-27 11:02:04 -0400

    Update pihole-unbound.sh

--- pihole-unbound.sh
+++ pihole-unbound.sh
@@ -1,22 +1,20 @@
- apt-get update
- apt-get upgrade
- 
+#!/bin/bash
 
- ufw allow 80/tcp
+apt-get update
+apt-get upgrade
+
+ufw allow 80/tcp
 ufw allow 53/tcp
 ufw allow 53/udp
 ufw allow 67/tcp
 ufw allow 67/udp
 ufw allow 546:547/udp
 
- adduser pihole
-
- usermod -aG sudo pihole
-
+adduser pihole
+usermod -aG sudo pihole
 
 sudo apt install unbound -y
 wget https://www.internic.net/domain/named.root -qO- | sudo tee /var/lib/unbound/root.hints
-
 
 # Define the path to the configuration file
 CONFIG_FILE="/etc/unbound/unbound.conf.d/pi-hole.conf"
@@ -34,8 +32,6 @@ sed -i '/private-address: 10.0.0.0\/8/d' $CONFIG_FILE
 echo "private-address: 192.168.0.0/24" >> $CONFIG_FILE
 
 sudo service unbound restart
-
-
 
 git clone --depth 1 https://github.com/pi-hole/pi-hole.git Pi-hole
 cd "Pi-hole/automated install/"
