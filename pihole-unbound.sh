e6d5bceb7b6beb565e12cf90cc9bc3cd3ac8d50f
daemonspectre <admin@family-p.com>
2024-06-27 12:03:50 -0400

    Update pihole-unbound.sh

--- pihole-unbound.sh
+++ pihole-unbound.sh
@@ -91,4 +91,9 @@ EOM
 
 # Write the configuration to the file
 echo "$CONFIG" > /etc/unbound/unbound.conf.d/pi-hole.conf
+
+git clone --depth 1 https://github.com/pi-hole/pi-hole.git Pi-hole
+cd "Pi-hole/automated install/"
+sudo bash basic-install.sh
+
 pihole -up
